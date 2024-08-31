bf2c651ec7790036186f41a92ac5ad8f43f98f9a
Ahmed Khan <khan.ahmed.m@outlook.com>
2024-08-31 13:04:17 -0500

    Added exception handling

--- src/main/python/bin/create_objects.py
+++ src/main/python/bin/create_objects.py
@@ -1,14 +1,24 @@
 from pyspark.sql import SparkSession
 
 def get_spark_object(envn,appName ):
-    if envn == 'TEST' :
-        master='local'
+    try:
+        if envn == 'TEST' :
+            master='local'
+        else:
+            master='yarn'
+        spark = SparkSession \
+                  .builder \
+                  .master(master) \
+                  .appName(appName) \
+                  .getOrCreate()
+    except NameError as exp:
+        print("NameError in the method - get_spark_object(). Please check the Stack Trace. " + str(exp))
+        raise
+    except Exception as exp:
+        print("Error in the method - get_spark_object(). Please check the Stack Trace. " + str(exp))
     else:
-        master='yarn'
-    spark = SparkSession \
-     .builder \
-     .master(master) \
-     .appName(appName) \
-     .getOrCreate()
+        print("Spark Object is created ...")
     return spark
+
+
 
--- src/main/python/bin/validations.py
+++ src/main/python/bin/validations.py
@@ -1,4 +1,19 @@
 
 def get_curr_date(spark):
-    opDF = spark.sql(""" select current_date """)
-    print("Validate the Spark object by printing Current Date - " + str(opDF.collect()))
+    try:
+        opDF = spark.sql(""" select current_date """)
+        print("Validate the Spark object by printing Current Date - " + str(opDF.collect()))
+    except NameError as exp:
+        print("NameError in the method - spark_curr_date(). Please check the Stack Trace. " + str(exp))
+        raise
+    except Exception as exp:
+        print("Error in the method - spark_curr_date(). Please check the Stack Trace. " + str(exp))
+        raise
+    else:
+        print("Spark object is validated. Spark Object is ready.")
+
+
+
+
+
+
