6f19d86632bb33962cdc846d4ad934aebbd78b5a
Ahmed Khan <khan.ahmed.m@outlook.com>
2024-09-09 00:09:35 -0500

    Added custom logger

--- src/main/python/bin/run_presc_pipeline.py
+++ src/main/python/bin/run_presc_pipeline.py
@@ -1,10 +1,12 @@
 ### Import all the necessary Modules
 import get_all_variables as gav
 from create_objects import get_spark_object
-from validations import get_curr_date
+from validations import get_curr_date, df_count, df_top10_rec
 import sys
 import logging
 import logging.config
+import os
+from presc_run_data_ingest import load_files
 
 ### Load the Logging Configuration File
 logging.config.fileConfig(fname='../util/logging_to_file.conf')
@@ -16,11 +18,47 @@ def main():
         spark = get_spark_object(gav.envn,gav.appName)
         # Validate Spark Object
         get_curr_date(spark)
-        # Set up Logging Configuration Mechanism
 
-        ### Initiate run_presc_data_ingest Script
+        ### Initiate presc_run_data_ingest Script
         # Load the City File
+        for file in os.listdir(gav.staging_dim_city):
+            print("File is "+ file)
+            file_dir = gav.staging_dim_city + '\\' + file
+            print(file_dir)
+            if file.split('.')[1] == 'csv' :
+                file_format = 'csv'
+                header=gav.header
+                inferSchema=gav.inferSchema
+            elif file.split('.')[1] == 'parquet' :
+                file_format = 'parquet'
+                header='NA'
+                inferSchema='NA'
+
+        df_city = load_files(spark = spark, file_dir = file_dir, file_format =file_format , header =header, inferSchema = inferSchema)
+
         # Load the Prescriber Fact File
+        for file in os.listdir(gav.staging_fact):
+            print("File is "+ file)
+            file_dir = gav.staging_fact + '\\' + file
+            print(file_dir)
+            if file.split('.')[1] == 'csv' :
+                file_format = 'csv'
+                header=gav.header
+                inferSchema=gav.inferSchema
+            elif file.split('.')[1] == 'parquet' :
+                file_format = 'parquet'
+                header='NA'
+                inferSchema='NA'
+        df_fact = load_files(spark = spark, file_dir = file_dir, file_format =file_format , header =header, inferSchema = inferSchema)
+
+        ### Validate run_data_ingest script for city Dimension & Prescriber Fact dataframe
+        df_count(df_city,'df_city')
+        df_top10_rec(df_city,'df_city')
+
+        df_count(df_fact,'df_fact')
+        df_top10_rec(df_fact,'df_fact')
+
+
         # Validate
         # Set up Error Handling
         # Set up Logging Configuration Mechanism
