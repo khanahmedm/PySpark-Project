1f7d04fbb4ba990d52caa36ace568f8d55e0dc7b
Ahmed Khan <khan.ahmed.m@outlook.com>
2024-09-16 12:03:05 -0500

    Added data pre-processing to clean years_of_exp field and convert it from string to number.

--- src/main/python/bin/presc_run_data_preprocessing.py
+++ src/main/python/bin/presc_run_data_preprocessing.py
@@ -1,6 +1,6 @@
 import logging
 import logging.config
-from pyspark.sql.functions import upper, lit
+from pyspark.sql.functions import upper, lit, regexp_extract, col
 
 # Load the Logging Configuration File
 logging.config.fileConfig(fname='../util/logging_to_file.conf')
@@ -25,13 +25,17 @@ def perform_data_clean(df1,df2):
         logger.info(f"perform_data_clean() is started for df_fact dataframe...")
         df_fact_sel = df2.select(df2.npi.alias("presc_id"),df2.nppes_provider_last_org_name.alias("presc_lname"), \
                              df2.nppes_provider_first_name.alias("presc_fname"),df2.nppes_provider_city.alias("presc_city"), \
-                             df2.nppes_provider_state.alias("presc_state"),df2.specialty_description.alias("presc_spclt"), df2.year_exp, \
+                             df2.nppes_provider_state.alias("presc_state"),df2.specialty_description.alias("presc_spclt"), df2.years_of_exp, \
                              df2.drug_name,df2.total_claim_count.alias("trx_cnt"),df2.total_day_supply, \
                              df2.total_drug_cost)
     #3 Add a Country Field 'USA'
         df_fact_sel = df_fact_sel.withColumn("country_name",lit("USA"))
     #4 Clean years_of_exp field
+        pattern='\d+'
+        idx=0
+        df_fact_sel = df_fact_sel.withColumn("years_of_exp",regexp_extract(col("years_of_exp"),pattern,idx))
     #5 Convert the yearS_of_exp datatype from string to Number
+        df_fact_sel = df_fact_sel.withColumn("years_of_exp",col("years_of_exp").cast("int"))
     #6 Combine First Name and Last Name
     #7 Check and clean all the Null/Nan Values
     #8 Impute TRX_CNT where it is null as avg of trx_cnt for the prescriber
